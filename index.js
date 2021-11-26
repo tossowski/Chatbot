ed4afa418e2c510666dccb099578bd103a9f3e16
tossowski <tossowski2012@gmail.com>
2021-11-26 17:06:23 -0500

    Added 3 agents

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
-'use strict';
+possible = ["./mark.html", "./alex.html", "./harold.html"]
+
+window.onload = function() {
+    let link = document.getElementById("alink");
 
 
-const {WebhookClient} = require('dialogflow-fulfillment');
-const express = require('express');
-//var https = require('https');
-const bodyParser = require('body-parser');
-var path = require('path');
-//var fs = require('fs');
+    // Update the current slider value (each time you drag the slider handle)
+    link.onclick = function() {
+        window.location.href = possible[Math.round(Math.random() * 2)]
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-// var options = {
-//     key: fs.readFileSync('./client-key.pem'),
-//     cert: fs.readFileSync('./client-cert.pem')
-// };
-
-function WebhookProcessing(req, res) {
-    const agent = new WebhookClient({request: req, response: res});
-
-    console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
-
-    console.log(JSON.stringify(req.body.queryResult.queryText));
-    console.log(JSON.stringify(req.body.queryResult.fulfillmentText));
+        
+    }
 
 }
-
-app.get('/', (req, res) => {
-    console.log("in get request")
-    res.sendFile(path.join(__dirname + '/index.html'));
-    
-});
-
-app.post('/fulfillment', function (req, res) {
-    res.sendFile(path.join(__dirname + '/pepe.html'));
-    console.log("in post request")
-    WebhookProcessing(req, res);
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
-
-//https.createServer(options, app).listen(PORT);
---
+++ main.js
@@ -0,0 +1,78 @@
+
+let generalQuestionPool = [];
+let intimateQuestionPool = [];
+
+let generalRobotResponses = [];
+let intimateRobotResponses = [];
+
+// Adding General Questions
+generalQuestionPool.push("What is your name?")
+generalQuestionPool.push("Where is your favorite place to eat?")
+generalQuestionPool.push("What do you usually order from there?")
+generalQuestionPool.push("Do you have any hobbies?")
+generalQuestionPool.push("What's your favorite item that you've bought this year?")
+
+// Adding intimate Questions
+generalQuestionPool.push("What is your biggest regret from college?")
+generalQuestionPool.push("What do you want to be remembered for if you passed away?")
+generalQuestionPool.push("Be honest, how often do you work from your bed?")
+generalQuestionPool.push("It was nice talking to you! I have to go to my meeting soon, so Goodbye!")
+
+
+
+// Adding Responses
+generalRobotResponses.push("Hello, my name is Mark.")
+generalRobotResponses.push("Nice to meet you!")
+generalRobotResponses.push("Interesting, maybe I should try going there sometime.")
+generalRobotResponses.push("That sounds delicious! I personally really love grilled cheese sandwiches.")
+generalRobotResponses.push("Cool, I usually spend my free time playing poker with other robots.")
+generalRobotResponses.push("That sounds nice, I might consider that in my holiday shopping plans.")
+
+// Adding intimate responses
+generalRobotResponses.push("I see, my biggest regret was not going to more parties.")
+generalRobotResponses.push("I understand. I would love to be remembered as a nice robot.")
+generalRobotResponses.push("Ok, because I like to sleep a lot.")
+
+
+let idx = 0;
+let intimate = false;
+let mystery_num = 0;
+
+window.onload = function() {
+    let responseButton = document.getElementById("responseButton");
+
+    // Update the current slider value (each time you drag the slider handle)
+    responseButton.onclick = function() {
+        if (idx == generalQuestionPool.length) {
+            let mys = document.getElementById("mysteryNum");
+            mys.innerHTML = "Your mystery number is: " + Math.floor(mystery_num / generalQuestionPool.length) + " Please use it to fill out this <a href=https://forms.gle/uKrJ8hh94gNewrrw5 target=_blank>survey</a>";
+            
+            return;
+        }
+        let response = document.getElementById("response").value;
+        document.getElementById("response").value = "";
+        mystery_num += response.split(" ").length;
+        
+        let agentResponseBox = document.getElementById("agentResponse");
+
+
+
+        agentres = generalRobotResponses[idx] + " " + generalQuestionPool[idx]
+        
+        agentResponseBox.innerHTML = "<b>Agent Response:</b> " + agentres;
+        var msg = new SpeechSynthesisUtterance();
+        // let voices = window.speechSynthesis.getVoices();
+        // window.speechSynthesis.getVoices().forEach(function(voice) {
+        //     console.log(voice.name, voice.default ? voice.default :'');
+        //   });
+          
+        //msg.voice = voices[2]; 
+        msg.text = agentres
+        window.speechSynthesis.speak(msg);
+
+        idx++;
+
+        
+    }
+
+}
